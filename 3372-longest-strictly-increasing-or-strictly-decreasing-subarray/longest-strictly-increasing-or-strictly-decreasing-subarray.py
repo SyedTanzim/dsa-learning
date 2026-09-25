c26086bc70407c920d2594946d0000c913e733e4
Syed Tanzim Wajih <syedtanzimwajih@gmail.com>
2026-09-25 16:54:41 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (64.78%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.py
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.py
@@ -1,26 +1,23 @@
 class Solution:
     def longestMonotonicSubarray(self, nums: List[int]) -> int:
-        longestIncreasingSubarray = 1
-        longestDecreasingSubarray = 1
-        length = 1
+        increasing = 1
+        decreasing = 1
+        lengthI = 1
+        lengthD = 1
 
         for i in range(1, len(nums)):
-            if nums[i] <= nums[i - 1]:
-                length = 1
+            if nums[i-1] < nums[i]:
+                lengthI += 1
+                if lengthI > increasing:
+                    increasing = lengthI
             else:
-                length += 1
-                if length > longestIncreasingSubarray:
-                    longestIncreasingSubarray = length
+                lengthI = 1
 
-        longestDecreasingSubarray = 1
-        length = 1
-        for i in range(1, len(nums)):
-            if nums[i] >= nums[i - 1]:
-                print(nums[i-1])
-                length = 1
+            if nums[i-1] > nums[i]:
+                lengthD += 1
+                if lengthD > decreasing:
+                    decreasing = lengthD
             else:
-                length += 1
-                if length > longestDecreasingSubarray:
-                    longestDecreasingSubarray = length
-        
-        return max(longestIncreasingSubarray, longestDecreasingSubarray)
+                lengthD = 1
+
+        return max(increasing, decreasing)
